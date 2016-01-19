ac846c159d9bccaf227e49af59a8020a6af6403a
Jonathan Chen <ypeels@users.noreply.github.com>
2016-01-19 14:02:02 -0600

    cleaned up vestigial code

--- a.sh
+++ a.sh
@@ -12,26 +12,15 @@ raw_moses=$testdir/IWSLT14.TED.tst2010.$lang1-$lang2.$lang1.tok.trans
 multevaldir=$(dirname $0)
 tokenizerdir=$multevaldir/tokenizers/europarl
 
-# adapted from MMM's train script
 function lowercase {
     output_file=$1.lower
-    #echo Lowercasing to $output_file # this fucks up the "return value"
     $tokenizerdir/lowercase.perl < $1 > $output_file
     echo $output_file
 }
 
 function tokenize {
     output_file=$1.tok
-    #echo Tokenizing to $output_file
-    
-    ## MMM version
-    #cat $1 | \
-    #    $tokenizerdir/tokenizer.perl -threads $(grep -c ^processor /proc/cpuinfo) -a -l $lang2 -q | \
-    #    $tokenizerdir/deescape-special-chars.perl > \
-    #    $output_file
-    
     $tokenizerdir/tokenizer.perl -q -l $lang2 < $1 > $output_file
-    
     echo $output_file
 }
 
