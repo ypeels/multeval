90f2444188dc4f95b83ff461d14b20169bb8af70
Jonathan Chen <ypeels@users.noreply.github.com>
2016-01-19 16:35:44 -0600

    cleaned up vestigial code

--- tokenizers/split_zh_chars.py
+++ tokenizers/split_zh_chars.py
@@ -8,30 +8,15 @@ import sys
 
 for ch in sys.stdin.read().decode('utf8'):
 
-    # no cannot just do this, because you don't want to split the non-Chinese characters
-    # ironically, this turned out to be EXACTLY what they did
-    #print ' '.join(list(line)),
+    # no cannot just do this, because you don't want to split the non-Chinese characters?
+    # NO, ironically, this turned out to be EXACTLY what they did
+    if not ch == '\r': # otherwise line count gets messed up for their gold, but NOT their MT output!?
+        sys.stdout.write(ch.encode('utf8') + ' ')    
     
-    # first try: space-delimit any non-latin characters
     # http://stackoverflow.com/questions/196345/how-to-check-if-a-string-in-python-is-in-ascii
     # http://stackoverflow.com/questions/255147/how-do-i-keep-python-print-from-adding-newlines-or-spaces
     
-    # ugh, this delimits accented Roman letters
-    #try:
-    #    ch.decode('latin_1')
-    #except UnicodeEncodeError:
-    
-    # nuke all non-zh characters
     #if ord(ch) >= 0x3038:
-    #    sys.stdout.write(' ' + ch.encode('utf8') + ' ')
-    #elif ch == '\n':
-    #    sys.stdout.write('\n')
-    #else:
-    #    pass#sys.stdout.write(ch.encode('utf8'))
-    
-    #if ord(ch) >= 0x3038:
-    if not ch == '\r': # otherwise line count gets messed up for their gold, but NOT their MT output!?
-        sys.stdout.write(ch.encode('utf8') + ' ')
     #else:
     #    sys.stdout.write(ch.encode('utf8'))
 
