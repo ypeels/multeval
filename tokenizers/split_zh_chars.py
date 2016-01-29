2f91949ab1d5ea85f8983fda3e67db719ffb1f31
Jonathan Chen <ypeels@users.noreply.github.com>
2016-01-28 20:35:28 -0600

    reversed spacing convention to work with my Moses translations

--- tokenizers/split_zh_chars.py
+++ tokenizers/split_zh_chars.py
@@ -11,7 +11,7 @@ for ch in sys.stdin.read().decode('utf8'):
     # no cannot just do this, because you don't want to split the non-Chinese characters?
     # NO, ironically, this turned out to be EXACTLY what they did
     if not ch == '\r': # otherwise line count gets messed up for their gold, but NOT their MT output!?
-        sys.stdout.write(ch.encode('utf8') + ' ')    
+        sys.stdout.write(' ' + ch.encode('utf8'))
     
     # http://stackoverflow.com/questions/196345/how-to-check-if-a-string-in-python-is-in-ascii
     # http://stackoverflow.com/questions/255147/how-do-i-keep-python-print-from-adding-newlines-or-spaces
